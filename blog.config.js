40fb6bc716f54cf86f26da41750fb19caba22c68
nadjaharold <rus0clov@gmail.com>
2020-09-02 21:25:39 +0900

    initial commit

---
+++ blog.config.js
@@ -0,0 +1,24 @@
+const BLOG = {
+  author: "harold",
+  title: "harold's diary",
+  description: "harold's diary",
+  summary: "a web developer in Tokyo",
+  language: "ja",
+  latestLimit: 4,
+
+  labels: {
+    default: "Blog",
+    latest: "Latest",
+    list: "Posts",
+  },
+
+  // email: "rus0clov@gmail.com",
+  github: "nadjaharold",
+  twitter: "nadjaharold",
+  domain: "harold.tokyo",
+
+  cn: false,
+  enableViews: false,
+};
+
+export default BLOG;
